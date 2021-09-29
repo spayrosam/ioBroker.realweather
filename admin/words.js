05ee0cd117c316176ee8fbfccc743c9982da50c0
ioBroker Adapter Creator <noreply@iobroker.net>
2021-09-29 11:32:18 +0200

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,113 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"realweather adapter settings": {
+		"en": "Adapter settings for realweather",
+		"de": "Adaptereinstellungen für realweather",
+		"ru": "Настройки адаптера для realweather",
+		"pt": "Configurações do adaptador para realweather",
+		"nl": "Adapterinstellingen voor realweather",
+		"fr": "Paramètres d'adaptateur pour realweather",
+		"it": "Impostazioni dell'adattatore per realweather",
+		"es": "Ajustes del adaptador para realweather",
+		"pl": "Ustawienia adaptera dla realweather",
+		"zh-cn": "realweather的适配器设置"
+	},
+	"Sound": {
+		"en": "Sound",
+		"de": "Klang",
+		"ru": "Звук",
+		"pt": "Som",
+		"nl": "Geluid",
+		"fr": "Sonner",
+		"it": "Suono",
+		"es": "Sonido",
+		"pl": "Dźwięk",
+		"zh-cn": "声音"
+	},
+	"Weather State": {
+		"en": "Weather State",
+		"de": "Wetterzustand",
+		"ru": "Состояние погоды",
+		"pt": "Estado do tempo",
+		"nl": "Weerstaat",
+		"fr": "État météorologique",
+		"it": "Stato del tempo",
+		"es": "Estado del tiempo",
+		"pl": "Stan pogody",
+		"zh-cn": "天气状态"
+	},
+	"myColor": {
+		"en": "myColor",
+		"de": "meineColor",
+		"ru": "Мой цвет",
+		"pt": "minhaCor",
+		"nl": "mijnKleur",
+		"fr": "maCouleur",
+		"it": "mioColore",
+		"es": "miColor",
+		"pl": "mójKolor",
+		"zh-cn": "我的颜色"
+	},
+	"myColor_tooltip": {
+		"en": "Description of\nmyColor",
+		"de": "Beschreibung von\nmyColor",
+		"ru": "Описание\nmyColor",
+		"pt": "Descrição de\nmyColor",
+		"nl": "Beschrijving van\nmyColor",
+		"fr": "Description de\nmyColor",
+		"it": "Descrizione di\nmyColor",
+		"es": "Descripción de\nmyColor",
+		"pl": "Opis\nmyColor",
+		"zh-cn": "\nmyColor的描述"
+	},
+	"htmlText": {
+		"en": "htmlText",
+		"de": "htmlText",
+		"ru": "htmlText",
+		"pt": "htmlText",
+		"nl": "htmlText",
+		"fr": "htmlText",
+		"it": "htmlText",
+		"es": "htmlText",
+		"pl": "htmlText",
+		"zh-cn": "htmlText"
+	},
+	"group_extraMyset": {
+		"en": "extraMyset",
+		"de": "extraMyset",
+		"ru": "extraMyset",
+		"pt": "extraMyset",
+		"nl": "extraMyset",
+		"fr": "extraMyset",
+		"it": "extraMyset",
+		"es": "extraMyset",
+		"pl": "extraMyset",
+		"zh-cn": "extraMyset"
+	},
+	"extraAttr": {
+		"en": "extraAttr",
+		"de": "extraAttr",
+		"ru": "extraAttr",
+		"pt": "extraAttr",
+		"nl": "extraAttr",
+		"fr": "extraAttr",
+		"it": "extraAttr",
+		"es": "extraAttr",
+		"pl": "extraAttr",
+		"zh-cn": "extraAttr"
+	},
+	"Instance": {
+		"en": "Instance",
+		"de": "Instanz",
+		"ru": "Инстанция",
+		"pt": "Instância",
+		"nl": "Instantie",
+		"fr": "Instance",
+		"it": "Esempio",
+		"es": "Instancia",
+		"pl": "Instancja",
+		"zh-cn": "例"
+	}
+};
